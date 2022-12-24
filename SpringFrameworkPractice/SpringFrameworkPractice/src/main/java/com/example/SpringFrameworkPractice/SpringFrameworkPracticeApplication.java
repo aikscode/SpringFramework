060e30c6858dfd3a214e3f8761770f04cbec2153
Aikins Dwamena <aikinsakenten@gmail.com>
2022-12-24 12:32:32 -0800

    Setting up spring project with jpa and h2 database

---
+++ SpringFrameworkPractice/SpringFrameworkPractice/src/main/java/com/example/SpringFrameworkPractice/SpringFrameworkPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringFrameworkPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkPracticeApplication.class, args);
+	}
+
+}
